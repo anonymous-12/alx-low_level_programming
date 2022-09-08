c0a85f7bdbdf5761903008f43a106029ed5e1476
Omua Egbokhare <omuamanda@gmail.com>
2022-09-08 15:28:14 -0700

    number 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Remains 0  
+ */
+int main(void)
+{
+	int n;
+	long int li;
+	long long int l;
+	char a;
+	float f;
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(n));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
