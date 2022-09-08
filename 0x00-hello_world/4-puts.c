a4cc6c1ceee8dab2600a34287937e652fe9c6844
Omua Egbokhare <omuamanda@gmail.com>
2022-09-08 14:55:25 -0700

    0-Hello_World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-/**main entry point
-
- * return = 0 because of the int in main entry
- 
-*/
-
+#include <stdio>
+/**
+ *Header with standard input
+ * next main entry point that returns 0
+ */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-  
+
+
