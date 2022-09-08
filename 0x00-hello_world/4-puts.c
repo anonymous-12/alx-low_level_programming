ff7dd8d4034ffa3c4741f6f0c037ac490bf211a3
Omua Egbokhare <omuamanda@gmail.com>
2022-09-08 14:31:06 -0700

    0-Hello_World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#includes <stdio.h>
+/**main entry point
+
+ * return = 0 because of the int in main entry
+ 
+*/
+
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle");
+  return 0;
+}
+  
