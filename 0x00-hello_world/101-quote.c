1e3b6908d2b980c71c3120413cd283a1606381b0
Omua Egbokhare <omuamanda@gmail.com>
2022-09-09 10:07:31 -0700

    stderr edit betty style

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char c[] = "and that piece of art is useful/ - Dora Korpar, 2015-10-19\n";
+	char c[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fprintf(stderr, "%s", c);
 	return (1);
