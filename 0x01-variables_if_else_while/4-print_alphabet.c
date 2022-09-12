03a0d258987ef1966f6ff1d90bea68410e163aaa
Omua Egbokhare <omuamanda@gmail.com>
2022-09-12 03:27:10 -0700

    Corrected Betty coding style

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print lowercase letters except q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z' lower++)
+	{
+
+	if (lower != 'e' && letter != 'q')
+	       putchar(lower);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
