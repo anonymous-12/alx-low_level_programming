37962ad6af9ac8456003be56949422b55840f072
Omua Egbokhare <omuamanda@gmail.com>
2022-09-12 08:24:51 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints lowercase alphabets in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'z'; lower >= 'a'; lower--)
+		putchar(lower);
+
+	putchar('\n');
+	return (0);
+}
