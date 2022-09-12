89dd1a675ecf1791be18e9d8e7a8614e175e3224
Omua Egbokhare <omuamanda@gmail.com>
2022-09-12 12:54:53 -0700

    Up to 11

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,10 +14,10 @@ int main(void)
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
-			putchar(num1 + '0');
+			putchar((num1 % 10)+ '0');
 			putchar(' ');
 			putchar((num2 / 10) + '0');
-			putchar(num2 + '0');
+			putchar((num2 % 10 )+ '0');
 
 			if (num1 == 98 && num2 == 99)
 				continue;
