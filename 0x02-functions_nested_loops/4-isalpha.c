92de46fb67905a9e51922068d91cf877bf92093e
Omua Egbokhare <omuamanda@gmail.com>
2022-09-16 12:34:16 -0700

    4-ALPHABET

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - checks letter
+ *
+ * Return: 1 if c is lowercase
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
