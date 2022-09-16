ee5b308064abbce60f75bb59de4aaeabeb506174
Omua Egbokhare <omuamanda@gmail.com>
2022-09-16 11:50:18 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *main - Prints _putchar followed by a new line
+ *
+ *Return: Remains 0
+ *
+ */
+
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+
+
+} 
