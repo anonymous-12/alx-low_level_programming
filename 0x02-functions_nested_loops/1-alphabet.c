a07c721cf438d850d9adec149c742b1002b04613
Omua Egbokhare <omuamanda@gmail.com>
2022-09-16 12:07:22 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main - Prints the alphabets in lowercase letters
+ *
+ *Return: 0 as per usual
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for(a = 'a'; a <= 'z'; a++)
+
+	{
+		_putchar(a);
+
+	}
+	_putchar('\n');
+}
