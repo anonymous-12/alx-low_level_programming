df9a59ffa25d3da88b2a1a5ab69b00069cb30849
Omua Egbokhare <omuamanda@gmail.com>
2022-09-18 04:13:48 -0700

    11-print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * print_to_98 - Prints all natural numbers from n to 98, followed by a 
+ * 		 new line.
+ * @n - the number to begin counting from
+ *
+ */
+void print_to_98(int n)
+{
+	if(n >= 98)
+	{
+	if(n > 98)
+	}
+}
