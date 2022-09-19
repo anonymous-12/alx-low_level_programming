170a22ed28f3c4ca4989f1410be9950be8029ea0
Omua Egbokhare <omuamanda@gmail.com>
2022-09-19 14:36:27 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h> 
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @str: the string to be printed
+ */
+void print_rev(char *s);
+{
+	int count = 0
+	
+	while (s[count] != 0)
+	{
+		count++;
+	}
+
+	while (i > 0)
+	{
+		count--;
+
+		putchar(s[count]);
+	}
+}
+
