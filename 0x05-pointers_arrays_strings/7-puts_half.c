97b2fbedc14d085136e089d127cef337993299c1
Omua Egbokhare <omuamanda@gmail.com>
2022-09-20 10:22:00 -0700

    Up to 11

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - prints half of a string followed by a new line
+ * @str: the string 
+ */
+void puts_half(char *str)
+{
+	int n;
+	int i;
+
+	while (str[n])
+	{
+		n++;
+	}
+
+	if (n % 2 == 1)
+	{
+		i = (n - 1) / 2;
+		i = n - i;
+	}
+
+	else
+	{
+		i = n / 2;
+	}
+
+	for (; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
