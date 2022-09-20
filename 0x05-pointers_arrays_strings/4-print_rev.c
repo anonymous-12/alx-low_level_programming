9463310e4573254ebac062e32b5c7ecf57579557
Omua Egbokhare <omuamanda@gmail.com>
2022-09-20 07:10:18 -0700

    Betty style documentation

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,19 @@
 #include "main.h"
-#include <stdio.h> 
+#include <stdio.h>
 /**
  * print_rev - prints a string, in reverse, followed by a new line
- * @str: the string to be printed
- * @i: iterator
+ * @s: the string to be reversed
  */
 void print_rev(char *s)
 {
 	int i;
-	
-	for (i = 0 ; s[i] != '\0' ; i++);
-	
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+
 	i--;
 
-	for (; i>= 0; i--)
+	for (; i >= 0; i--)
 
 		putchar(s[i]);
 	putchar('\n');
