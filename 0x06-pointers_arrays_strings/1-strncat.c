4499be052908e11209383afd2d9fece8c822a2c6
Omua Egbokhare <omuamanda@gmail.com>
2022-09-22 11:36:26 -0700

    Concatenate strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - concatenates a string and uses the n byte.
+ * @dest: a string
+ * @ src: a string 
+ * @n: a variable
+ * Return: Concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i++])
+		j++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[j++] = src[i];
+
+	return (dest);
+}
