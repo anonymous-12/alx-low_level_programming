52826334d11dbe408d5451c29ee2d5d3b03d88cb
Omua Egbokhare <omuamanda@gmail.com>
2022-09-22 13:26:34 -0700

    Compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: a string to be compared
+ * @s2: same as above
+ * Return: return -1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*a != '\0' && *b != '\0')
+	{
+		if (*a != *b)
+		{
+			i = 1;
+		}
+		a++;
+		b++;
+	}
+}
