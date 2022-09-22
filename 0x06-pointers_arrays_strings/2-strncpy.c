034bc064ad671b3cb4aeb26b5b44243cce837f82
Omua Egbokhare <omuamanda@gmail.com>
2022-09-22 12:49:37 -0700

    Copy strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * strncpy - copies a string
+ * @dest: stores copy string
+ * @src: string to be copied
+ * Return: a pointer to dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	while (src[i++])
+		j++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+
+	for (i = j; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
