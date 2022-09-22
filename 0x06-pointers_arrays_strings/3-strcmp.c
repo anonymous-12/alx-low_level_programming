7d996edf779d4b5608323bdb28d9b36ac400e84e
Omua Egbokhare <omuamanda@gmail.com>
2022-09-22 13:31:50 -0700

    Compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,18 +7,11 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (*s1 != '\0' && *s2 != '\0')
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-		{
-			i = 1;
-		}
+
 		s1++;
 		s2++;
 	}
-	return (0);
-	else
-	return (1);
+	return (*s1 - *s2);
 }
