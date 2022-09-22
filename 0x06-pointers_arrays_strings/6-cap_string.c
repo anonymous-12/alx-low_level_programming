247cb4b9e452a4a30c3e4cb67a2a9fc974e48ab9
Omua Egbokhare <omuamanda@gmail.com>
2022-09-22 14:14:47 -0700

    capitalizes small letters

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - capitalizez allwords of a string
+ * @s: the string to be capitalized
+ * Return: 0
+ */
+char *cap_string(char *)
+{
+	int i = 1;
+	
+	if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] = s[0] - 32;
+	}
+	for (; s[i]; i++)
+	{
+		
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t'
+		    || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '!'
+		    || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '('
+		     || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}'
+		     || s[i - 1] == '.') && (s[i] > 'a' && s[i] < 'z'))
+		{
+			s[i] = s[i] - 32;
+		}	
+	}
+	return (s);
+}
