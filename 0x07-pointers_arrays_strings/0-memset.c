1d4b4f9f0b77abde81996b37d3b5fc7b148c85af
Omua Egbokhare <omuamanda@gmail.com>
2022-09-26 03:49:33 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * dest - destination array
+ * src - source of data to be copied
+ * @n: number of bytes to be copied
+ * Return: Returns apointer to dest
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
