db7bf36912d6cbc978a797ec1b1e38a65c01b3ed
Omua Egbokhare <omuamanda@gmail.com>
2022-09-27 07:17:29 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,13 @@
  */
 char *_strchr(char *s, char c);
 {
-	int i;
-	i = 0;
-
-	while (s[i] != '\0')
+	while (*s)
 	{
-		if (s[i] == c)
-			return ((s + i));
-		i++;
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
 	}
-	if (c == '\0')
-		return ((s + i));
-	return (NULL);
+	return ('\0');
 }
