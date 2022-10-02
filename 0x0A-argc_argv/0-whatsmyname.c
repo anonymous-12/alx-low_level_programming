49128e06389fe97b237c5d3d3dbfeaabfdd7b37b
Omua Egbokhare <omuamanda@gmail.com>
2022-10-02 09:32:50 -0700

    whats my name?

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/** 
+  * whatsmyname - Prints the name of a program followed by a new line
+  * @argc: The number of arguments supplied to the program
+  * @argv: An array of pointers to the arguments.
+  *
+  *Return: Zero.
+  */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return(0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+How to use arguments passed to my program
+argc and argv functions 
