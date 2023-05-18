642c1995559186f1a82b1ec26164e6bd8f00c7cb
omuamanda <omuamanda@gmail.com>
2023-05-18 23:25:22 +0100

    all

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Main Function - Prints a string tostandard output
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
