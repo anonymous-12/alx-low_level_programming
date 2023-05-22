ba53ba24f020bd2b15e2a621c15c8c9901bc7983
omuamanda <omuamanda@gmail.com>
2023-05-22 14:35:15 +0100

    desrever tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char rev_alphabet;
 
-	for (rev_alphabet = 'z'; rev_alphabet <= 'a'; rev_alphabet--)
+	for (rev_alphabet = 'z'; rev_alphabet >= 'a'; rev_alphabet--)
 		putchar(rev_alphabet);
 	putchar('\n');
 
