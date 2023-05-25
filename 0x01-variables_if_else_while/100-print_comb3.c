5c02254ab0209d8b2d38996b451f531045cd93b4
omuamanda <omuamanda@gmail.com>
2023-05-25 17:01:17 +0100

    100 combos

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two digits
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			putchar((num1 % 10) + '0');
+			putchar ((num2 % 10) + '0');
+
+			if (num1 == 9 && num2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
