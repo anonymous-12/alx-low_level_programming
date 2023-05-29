00ab38f0c95b8dbfda18809a00988ffbd3085ce6
omuamanda <omuamanda@gmail.com>
2023-05-29 22:56:17 +0100

    index += 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * puts2 - Prints every other character of a string, starting with the first
- * @s: the string
+ * @str: the string
  */
-void puts2(char *s)
+void puts2(char *str)
 {
 	int index = 0;
 
