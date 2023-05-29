a1070cb5a7c238e9d611d587d466b6b190a01b2e
omuamanda <omuamanda@gmail.com>
2023-05-29 21:14:34 +0100

    swaping is fun

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: the first integer
+ * @b: the second integer
+ * Return: 0(success)
+ */
+void swap_int(int *a, *b)
+{
+	int store = *a;
+	*a = *b;
+	*b = store;
+}
