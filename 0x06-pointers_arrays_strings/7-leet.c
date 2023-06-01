0da74a6bceba83769637be6112828b443dcaf028
omuamanda <omuamanda@gmail.com>
2023-06-01 16:00:47 +0100

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet -  encodesa string into 1337
+ * @str: the string to be encoded
+ * Return: a pointer to str
+ */
+char *leet(char *str)
+{
+	int indx1 = 0, indx2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[count])
+	{
+		for index = 0; index <= 7; index++)
+		{
+			if (str[count] == leet[index] || str[count] - 32 == leet[index])
+				str[count] = index + '0';
+		}
+		count++
+	}
+
+	return (str);
+}
