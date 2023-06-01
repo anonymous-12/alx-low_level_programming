824552ea6d5e07ad6925a15b50d1768d9ac48153
omuamanda <omuamanda@gmail.com>
2023-06-01 15:06:53 +0100

    betty errors

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,20 +9,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *ptr = dest;
+	int i = 0, s_len = 0;
 
-	while (*src != '\0')
-	{
-		*ptr = *src;
-		ptr++;
-		src++;
-		n--;
-	}
-	while (n > 0)
-	{
-		*ptr = '\0';
-		ptr++;
-		n--;
-	}
+	while (src[i++])
+		s_len++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+	for (i = s_len; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
