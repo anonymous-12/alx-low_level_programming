e8fdc8fae70b3db735279a41fd7491f87c4ccb28
omuamanda <omuamanda@gmail.com>
2023-06-01 16:30:48 +0100

    rot13

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,23 +4,4 @@
  *
  *Return: a pointer to str
  */
-char *leet(char *)
-{
-	int count, j;
-	char m[] = {'a', 'e', 'o', 't', 'l', '\0'};
-	char n[] = {'A', 'E', 'O', 'T', 'L', '\0'};
-	char numbers[] = {'4', '3', '0', '7', '1', '\0'};
-
-	for (count = 0; changed[count] != '\0'; ++count)
-	{
-		for (j = 0; j < 5; j++)
-		{
-			if (changed[count] == m[j] || changed[count] == n[j])
-			{
-				changed[count] = numbers[j];
-			}
-		}
-	}
-	return (changed);
-}
 
